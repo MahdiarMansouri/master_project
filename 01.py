b14dfc52c381acab4d03f73c303312204bf4b8a7
MahdiarMansouri <mahdiarm60@gmail.com>
2024-03-24 17:39:44 +0330

    add train results of efficientnet on 6000 images

--- 01.py
+++ 01.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 
 class SplitImageDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, transform=None, split_number=3):
         """
         Args:
             root_dir (string): Directory with all the images and subdirectories as classes.
@@ -20,14 +20,13 @@ class SplitImageDataset(Dataset):
         # Load the dataset using ImageFolder
         self.dataset = datasets.ImageFolder(root=root_dir)
 
-        # Create the (image, label) pairs, where each image is split into 9 parts.
+        # Create the (image, label) pairs, where each dimension of image is split into (split_number) parts.
         for img, label in self.dataset:
             width, height = img.size
-            step_w, step_h = width // 4, height // 4
+            step_w, step_h = width // split_number, height // split_number
 
-            for i in range(4):
-                for j in range(4):
-
+            for i in range(split_number):
+                for j in range(split_number):
                     left = i * step_w
                     upper = j * step_h
                     right = (i + 1) * step_w
@@ -49,11 +48,12 @@ class SplitImageDataset(Dataset):
 
 
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # Resize each segment for your model input
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
 ])
 
-split_dataset = SplitImageDataset(root_dir='D:\Master Project\model\model-1\Corallo-vs-Myxo\\train', transform=transform)
+split_dataset = SplitImageDataset(root_dir='D:\Master Project\model\model-1\Corallo-vs-Myxo\\train',
+                                  transform=transform, split_number=4)
 dataloader = DataLoader(split_dataset, batch_size=32, shuffle=True)
 
 print(split_dataset.__len__())
